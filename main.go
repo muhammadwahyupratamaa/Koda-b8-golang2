ddb30fc9b320a0d40d9a34a4bb6bfe846cade394
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-06 15:19:08 +0700

    feat:complete golang nested struct and slice exercise answer quetiom 5

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// soal 1
 type We struct{
 	are Are
 }
@@ -12,9 +13,30 @@ type The struct{
 	best string 
 }
 
+// Soal 2
+
 type Hello struct{
 	world string
 }
+
+
+
+// type Obj struct {
+// 	str [] 
+// }
+
+// type Academy struct {
+// 	academy string
+// }
+
+
+// Soal 5
+type Number struct {
+	first [] int
+	second [] int
+}
+
+
 
 
 func main() {
@@ -30,6 +52,17 @@ hello := Hello{
 	world : "Hello World",
 }
 
+num := Number{
+	first: []int{
+		10,15,30,
+	},
+	second: []int{
+		10,17,17,
+
+	},
+}
+
 fmt.Println(we.are.the.best)
 fmt.Println(hello.world)
+fmt.Println(num.first[1] + num.second[2])
 }
