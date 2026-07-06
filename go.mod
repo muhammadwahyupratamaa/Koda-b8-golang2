de9dca8bd8286d5f5dc9011ecb002629169a033f
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-06 14:26:17 +0700

    feat: initialize module and add main program and answer question 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module koda-b8-golang2
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type We struct{
+	are Are
+}
+type Are struct{
+	the The	
+}
+type The struct{
+	best string 
+}
+
+func main() {
+we := We{
+	are: Are{
+		the: The{
+			best: "KODA",
+		},
+	} ,
+}
+
+fmt.Println(we.are.the.best)
+}
